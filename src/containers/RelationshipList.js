1d877d8f3b5f24baaf1eace1c32df92fac793542
Adam Gray <adam@pateo.co>
2018-05-24 12:38:59 +0100

    Loads in correct relationships for ticket id

--- src/containers/RelationshipList.js
+++ src/containers/RelationshipList.js
@@ -48,6 +48,17 @@ class RelationshipList extends Component {
               description: "The google phone"
             }
           }
+        },
+        {
+          id: "61c96980-5f42-11e8-9594-2d151af981d8",
+          target: "703a6c3e-5f41-11e8-9594-bf751b51d8d3",
+          resource: {
+            type: "event",
+            attributes: {
+              name: "Summer Party",
+              location: "London"
+            }
+          }
         }
       ]
     }
@@ -55,6 +66,12 @@ class RelationshipList extends Component {
 
   componentDidMount() {
     let ticketID = 0
+    let relationshipTypes = []
+    let relationships = []
+
+    this.setState({
+      isLoading: true
+    })
 
     window.client.get('ticket.id')
       .then((res) => {
@@ -63,18 +80,64 @@ class RelationshipList extends Component {
         return window.client.request('/api/custom_resources/relationship_types')
       })
       .then((res) => {
-        this.setState({
-          relationshipTypes: res.data.map((item) => {
-            return {
-              key: item.key
-            }
+        relationshipTypes = res.data.map((item) => {
+          return {
+            key: item.key
+          }
+        })
+
+        return Promise.all(
+          relationshipTypes.map((relationshipType) => {
+            return window.client.request(`/api/custom_resources/resources/zen:ticket:${ticketID}/relationships/${relationshipType.key}`)
           })
+        )
+      })
+      .then((res) => {
+        relationships = res.filter((relationship) => {
+          if (relationship.data.length > 0) return relationship
+        }).reduce((prev, current) => {
+          return prev.concat(current.data.map((item) => ({ id: item.id, target: item.target })))
+        }, [])
+
+        return Promise.all(
+          relationships.map((relationship) => {
+            return window.client.request(`/api/custom_resources/resources/${relationship.target}`)
+          })
+        )
+      })
+      .then((res) => {
+        let resources = res.map((resource) => ({
+          type: resource.data.type,
+          attributes: resource.data.attributes
+        }))
+
+        relationships.map((relationship, index) => {
+          relationship.resource = resources[index]
+          return relationship
+        })
+
+        this.setState({
+          isLoading: false,
+          relationshipTypes: relationshipTypes,
+          relationships: relationships
         })
       })
       .catch((err) => console.log(err))
   }
 
+  handleDeleteRelationship(id) {
+    console.log(id)
+  }
+
   render() {
+
+    let relationshipList = this.state.relationships.map((relationship) => (
+      <div key={relationship.id}>
+        <p>{relationship.resource.type}</p>
+        <p>{relationship.resource.attributes.name}</p>
+        <button onClick={this.handleDeleteRelationship.bind(this, relationship.id)}>Delete</button>
+      </div>
+    ))
 
     if (this.state.isLoading) {
       return (
@@ -84,6 +147,9 @@ class RelationshipList extends Component {
 
     return (
       <div>
+        <div>
+          {relationshipList}
+        </div>
         <div>
           <Button type='default'>
             <Icon type="plus" /> Attach a resource
